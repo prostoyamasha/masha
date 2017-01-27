68e4087e72a04ba5e4166eda53179feff87ef9e8
masha <masha@masha.masha>
2017-01-27 03:41:35 +0300

    dz

---
+++ variable.php
@@ -0,0 +1,14 @@
+<head>
+<title>PHP-dz</title>
+</head>
+<body>
+<?php
+$age = '29';
+$name = 'Маша';
+$date = "Меня зовут $name мне $age годиков";
+
+$data = "Меня зовут ".$name." мне ".$age." годиков";
+echo "$data</br>";
+echo "$date";
+?>
+</body>
